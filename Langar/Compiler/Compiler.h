ad16a50bcf539403493d216278f78fa4c95e7938
Inbar <inbaravital4@gmail.com>
2020-12-02 18:07:12 +0200

    just organizing the compiler's header file

--- Langar/Compiler/Compiler.h
+++ Langar/Compiler/Compiler.h
@@ -9,17 +9,21 @@
 #include "../Shunting-yard/ShuntingYard.h"
 
 char* compileClass(Class* class);
-char* compileFunc(Func* func);
 char* compileExpression(LexLine* expression, Func* func);
-Var newVar(LexLine* line);
-Var initVar(LexLine* line);
-Func newFunc(LexLine* line);
-Func initFunc(LexLine* line);
-Var* getVar(char* name,  Func* func);
 char* compileReturn(LexLine* line, Func* func);
-void compileInitVar(Func* func, int index);
 char* getSizePtr(Var* var);
+
+// Variables
+Var initVar(LexLine* line);
+Var* getVar(char* name,  Func* func);
+void compileInitVar(Func* func, int index);
 char* compileUpdateLocalVar(Func* func, int index);
+
+// Functions
+char* compileFunc(Func* func);
+Func initFunc(LexLine* line);
+
+// Conditions
 char* compileCondition(Func* func, int index);
 int getConditionLength(Func* func, int index);
 int getInsideCondition(Func* func, int index);
